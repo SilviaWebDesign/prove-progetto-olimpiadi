6c80f1c20c49b2bf8a4157dbda779a092e26b113
letizianeri <letizia.neri@mail.polimi.it>
2026-06-03 10:35:57 +0200

    pulizia pagine vecchie

--- src/routes/infrastrutture/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-  import SectionPage from '$lib/components/file-chiara/SectionPage.svelte';
-  import { sections } from '$lib/components/file-chiara/sections.js';
-</script>
-
-<SectionPage section={sections.infrastructure} />
